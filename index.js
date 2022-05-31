834646702ac2563799cf917f5d4a673a31832692
RIMESBEAT <elijahisaiah44@gmail.com>
2022-05-31 11:33:01 +0200

    new design added

---
+++ index.js
@@ -0,0 +1,8 @@
+var myName = "kings beat";
+myName.length;
+
+var alphaLetters = prompt("compose ur message here:");
+var letterCount = alphaLetters.length;
+alert(
+  "You have type" + letterCount + "You have "(200 - letterCount) + "remaining"
+);

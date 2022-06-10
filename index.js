cacc7dead6a5c6e1347d0d3d5845149ca8366087
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2022-06-10 15:54:47 -0700

    newest update

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 var myName = "kings beat";
 myName.length;
-
-var alphaLetters = prompt("compose ur message here:");
-var letterCount = alphaLetters.length;
-alert(
-  "You have type" + letterCount + "You have "(200 - letterCount) + "remaining"
-);
